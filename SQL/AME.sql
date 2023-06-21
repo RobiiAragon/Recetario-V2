6cc3211e8444c3c07475b91d14a50548a558dd00
Robii Aragon <55045932+RobiiAragon@users.noreply.github.com>
2023-06-20 18:41:38 -0700

    Update AME.sql

--- SQL/AME.sql
+++ SQL/AME.sql
@@ -1,23 +1,153 @@
-CREATE DATABASE AME;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 21, 2023 at 03:41 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
-CREATE TABLE pacientes (
-  id int(11) NOT NULL,
-  nombre varchar(50) NOT NULL,
-  fecha_naciemiento date DEFAULT NULL,
-  direccion varchar(100) DEFAULT NULL,
-  telefono varchar(20) DEFAULT NULL
-);
-
-
-CREATE TABLE tratamientos (
-  id int(11) NOT NULL,
-  nombre varchar(200) DEFAULT NULL,
-  descripcion varchar(200) DEFAULT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE usuarios (
-  id int(11) NOT NULL,
-  usuario varchar(50) NOT NULL,
-  contraseña varchar(100) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ame`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `fecha_naciemiento` date DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recetas`
+--
+
+CREATE TABLE `recetas` (
+  `receta_id` int(11) NOT NULL,
+  `fecha` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `edad` varchar(255) DEFAULT NULL,
+  `temp` varchar(255) DEFAULT NULL,
+  `ta` varchar(255) DEFAULT NULL,
+  `peso` varchar(255) DEFAULT NULL,
+  `fc` varchar(255) DEFAULT NULL,
+  `talla` varchar(255) DEFAULT NULL,
+  `fr` varchar(255) DEFAULT NULL,
+  `circun_abdom` varchar(255) DEFAULT NULL,
+  `id` varchar(255) DEFAULT NULL,
+  `alergias` varchar(255) DEFAULT NULL,
+  `tratamiento` varchar(255) DEFAULT NULL,
+  `indicaciones_generales` varchar(255) DEFAULT NULL,
+  `proxima_cita` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tratamientos`
+--
+
+CREATE TABLE `tratamientos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(200) DEFAULT NULL,
+  `descripcion` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `contraseña` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `contraseña`) VALUES
+(1, 'Aragon', 'jala1975');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `recetas`
+--
+ALTER TABLE `recetas`
+  ADD PRIMARY KEY (`receta_id`);
+
+--
+-- Indexes for table `tratamientos`
+--
+ALTER TABLE `tratamientos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `recetas`
+--
+ALTER TABLE `recetas`
+  MODIFY `receta_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+
+--
+-- AUTO_INCREMENT for table `tratamientos`
+--
+ALTER TABLE `tratamientos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
